a8670f8484c866a76f131b09eafd3785e7c17a96
ValerijaDrozda <lerusyadrr@gmail.com>
2023-03-06 00:59:46 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,51 @@
-# python3
+## Valerija Drozda 221RDB450
 
 import sys
 import threading
 import numpy
 
-
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    
+    empty = numpy.zeros(n)
+
+    for i in range(n):
+        if parents[i] != -1:
+            index = parents[i]
+            empty[index] = 1
+    
+    answer = int(np.numpy(empty) + 1)
+    print(answer)
+
 
 
 def main():
-    # implement input form keyboard and from files
     
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    text = input()
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+    if "I" in text:
+        
+        n = int(input())
+        
+        string = input()
+        parents = numpy.asarray(list(map(int, string.split())))
+
+        if n == parents.size:
+            compute_height(n, parents)       
+    
+    elif "F" in text:
+        file_n = input().strip()
+        if "a" in file_n:
+            return
+        else:
+            with open('test/' + file_n, 'r') as f:
+                
+                n = int(f.readline())
+                
+                string = f.readline()
+                parents = numpy.asarray(list(map(int, string.split())))
+
+                if n == parents.size:
+                    compute_height(n, parents)
+    
+   
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
